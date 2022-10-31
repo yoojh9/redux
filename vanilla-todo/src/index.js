56ffcc70f192b8aa11ee992d027a7aa11498966c
yoojh9 <yoojh9@gmail.com>
2022-10-31 17:40:21 +0900

    add Redux

--- vanilla-todo/src/index.js
+++ vanilla-todo/src/index.js
@@ -1,19 +1,36 @@
 
+import {createStore} from "redux";
+
 const form = document.querySelector('form');
 const input = document.querySelector('input');
 const ul = document.querySelector('ul');
 
-const createToDo = toDo => {
-  const li = document.createElement('li');
-  li.innerText = toDo;
-  ul.appendChild(li);
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const reducer = (state = [], action) => {
+  console.log(action)
+  switch (action.type) {
+    case ADD_TODO: return [...state, action.text]; 
+    case DELETE_TODO: return []
+    default:
+      return state;
+  }
 }
+
+const store = createStore(reducer);
+
+const onChange = () => {
+  ul.innerText = store.getState();
+}
+store.subscribe(onChange);
 
 const onSubmit = e => {
   e.preventDefault();
   const toDo = input.value;
   input.value = "";
-  createToDo(toDo)
+  //createToDo(toDo);
+  store.dispatch({type: ADD_TODO, text: toDo});
 }
 
 form.addEventListener('submit', onSubmit);
